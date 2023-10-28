fd298770eff4b25eda322edb878b70812747cfa1
Adem TONAY <ademtonay@gmail.com>
2023-10-28 15:19:59 +0300

    chore(config): add postgres
    Signed-off-by: Adem TONAY <ademtonay@gmail.com>

--- src/config/default.ts
+++ src/config/default.ts
@@ -3,4 +3,23 @@ export default {
 		env: 'development',
 		port: 3000,
 	},
+	database: {
+		postgres: {
+			host: '127.0.0.1',
+			port: 5432,
+			username: 'service_user',
+			password: '123456',
+			dbName: 'development',
+			charset: 'utf8',
+			collate: 'utf8_general_ci',
+			pool: {
+				max: 10,
+				min: 0,
+				acquire: 30000,
+				idle: 10000,
+				maxUses: 10,
+			},
+			timezone: '+02:00',
+		},
+	},
 }
