0ecb9182da0a828e4926a51489670ba3fe2b1595
Adem TONAY <ademtonay@gmail.com>
2023-10-28 14:48:48 +0300

    feat(database): implement Database class for Sequelize connection management
    - Introduce Database class extending Sequelize for tailored connection setup.
    - Add constructor to handle custom connection URIs based on provided options and environment.
    - Implement connect method to authenticate and sync database tables.
    - Incorporate PrettyLogger for logging database connection activities.
    
    Signed-off-by: Adem TONAY <ademtonay@gmail.com>

---
+++ src/libs/Database.ts
@@ -0,0 +1,43 @@
+import config from 'config'
+import { Sequelize, Options } from 'sequelize'
+import PrettyLogger from '../utils/PrettyLogger'
+
+export type DatabaseOptions = Options & {
+	dbName: string
+	useCustomPort?: boolean
+}
+
+const { env } = config.get<{ env: string }>('server')
+
+export default class Database extends Sequelize {
+	/**
+	 * Constructor for the Database class.
+	 * @param {DatabaseOptions} options - The options for configuring the database connection.
+	 */
+	constructor(readonly options: DatabaseOptions) {
+		let dbUri = `${options.dialect}://${options.host}/${options.dbName}`
+
+		// Use custom port if specified in options
+		if (options.useCustomPort || (env === 'development' && options.port)) {
+			dbUri = `${options.dialect}://${options.host}:${options.port}/${options.dbName}`
+		}
+
+		super(dbUri, options)
+	}
+
+	/**
+	 * Connect to the database, authenticate, and sync tables.
+	 * @returns {Promise<void>}
+	 */
+	public async connect(): Promise<void> {
+		try {
+			await this.authenticate()
+			PrettyLogger.info('Database is authenticated successfully.')
+
+			await this.sync({ alter: true })
+			PrettyLogger.info('Database and tables synced.')
+		} catch (err) {
+			PrettyLogger.error(err, 'Database Connection Error:')
+		}
+	}
+}
