c8552f6a60145f2bf1c5068f39b7372ff825d84e
Adem TONAY <ademtonay@gmail.com>
2023-10-29 14:16:46 +0300

    chore(testing): setup ts-jest configuration
    Signed-off-by: Adem TONAY <ademtonay@gmail.com>

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	testMatch: ['**/*.test.ts'], // This line ensures that only files with the .test.ts extension are run as tests.
+}
